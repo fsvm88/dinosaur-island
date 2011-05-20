3cf31b268876df8e987e6121676c256f0ee380d1
fsvm88 <fsvm88@c12e17a0-9a89-4ea4-9972-a45f88b339be>
2011-05-20 07:28:46 +0000

    Espandi i tipi di Cella Vegetazione e Carogna per contenere informazioni addizionali. Crea nuova classe CellaConDinosauro per gestire una cella occupata dal dinosauro. Questa contiene anche una referenza alla Cella che il dinosauro sta occupando.

--- trunk/src/dinolib/Carogna.java
+++ trunk/src/dinolib/Carogna.java
@@ -2,5 +2,58 @@ package dinolib;
 
 
 public class Carogna extends Cella {
+	/**
+	 * Contiene il valore iniziale dell'energia massima ricavabile dalla carogna.
+	 * @uml.property  name="valoreIniziale"
+	 */
+	private int valoreIniziale;
 
+	/**
+	 * Contiene il valore attuale della massima energia ricavabile dalla carogna.
+	 * @uml.property  name="valoreAttuale"
+	 */
+	private int valoreAttuale;
+
+	/**
+	 * Getter of the property <tt>valoreAttuale</tt>
+	 * @return  Returns the valoreAttuale.
+	 * @uml.property  name="valoreAttuale"
+	 */
+	public int getValoreAttuale() {
+		return valoreAttuale;
+	}
+
+	/**
+	 * Il costruttore si occupa di inizializzare il valore di tettoMassimo.
+	 */
+	public Carogna(int valoreIniziale){
+		this.valoreIniziale = valoreIniziale;
+		this.valoreAttuale = valoreIniziale;
+	}
+	
+	/**
+	 * Aggiorna il valore attuale della cella al passare dei turni di gioco
+	 */
+	public void aggiornaCella() {
+		int diffTemp = valoreAttuale - (valoreIniziale/20);
+		if (diffTemp <= 0) {
+			valoreAttuale = 0;
+		}
+		else if (diffTemp > 0) {
+			valoreAttuale = diffTemp;
+		}
+	}
+
+	/**
+	 * Aggiorna il valore della cella quando questa viene mangiata.
+	 */
+	public void mangia(int valoreMangiato) {
+		int diffTemp = valoreAttuale - valoreMangiato;
+		if (diffTemp <= 0) {
+			valoreAttuale = 0;
+		}
+		else if (diffTemp > 0) {
+			valoreAttuale = diffTemp;
+		}
+	}
 }
