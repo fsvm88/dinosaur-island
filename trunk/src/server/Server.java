46a4a20d731a88c81f3edf757b854017afbefd80
fsvm88 <fsvm88@c12e17a0-9a89-4ea4-9972-a45f88b339be>
2011-05-24 15:09:03 +0000

    Aggiungi @SuppressWarning a caricaFileGiocatori.

--- trunk/src/server/Server.java
+++ trunk/src/server/Server.java
@@ -1,11 +1,9 @@
 package server;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.util.Hashtable;
@@ -119,6 +117,7 @@ public class Server {
 	 * @throws IOException 
 	 * @throws ClassNotFoundException 
 	 */
+	@SuppressWarnings("unchecked")
 	private void caricaFileGiocatori(String nomefile) throws IOException, ClassNotFoundException, FileNotFoundException {
 		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(nomefile)));
 		Giocatori = (Hashtable<String, Giocatore>) ois.readObject();
