c1c06eacf68bebfac6f51557748ab53a8f7229aa
fsvm88 <fsvm88@c12e17a0-9a89-4ea4-9972-a45f88b339be>
2011-05-25 13:38:16 +0000

    Sposta tutto quello che serve da Server a Logica. Fixa le referenze in Server. ClientWorker è ancora da fixare.

--- trunk/src/dinolib/Logica.java
+++ trunk/src/dinolib/Logica.java
@@ -1,6 +1,106 @@
 package dinolib;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Hashtable;
+import java.util.Random;
+
 import dinolib.MieLogicExceptions;
 
-public class Logica {	
+public class Logica {
+	/**
+	 * Definisce staticamente il lato della mappa.
+	 * @uml.property name="LATO_MAPPA"
+	 */
+	private final int lato_MAPPA = 40;
+	/**
+	 * Definisce il riferimento alla mappa.
+	 * @uml.property name="rifMappa"
+	 */
+	private Mappa rifMappa;
+	/**
+	 * Definisce la lista dei giocatori.
+	 * @uml.property name="Giocatori"
+	 */
+	private Hashtable<String, Giocatore> Giocatori;
+	/**
+	 * Definisce la stringa che contiene il nome del giocatore che in questo momento ha il turno.
+	 * @uml.property name="Giocatori"
+	 */
+	private String nomeGiocatoreCorrente;
+	/**
+	 * Definisce una variabile che assicura che qualcuno sta giocando.
+	 * @uml.property name="qualcunoStaGiocando"
+	 */
+	protected boolean qualcunoStaGiocando = false;
+	/**
+	 * Definisce il generatore di numeri casuali.
+	 * @uml.property name="rnd"
+	 */
+	Random rnd = new Random();
+	
+	public Logica () {
+		try {
+			caricaPartitaDaFile();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("No save files found, creating a new map..");
+			creaNuovaMappa();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("IOException while trying to open save files. Exiting..");
+			System.exit(-1);
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("ClassNotFoundException while trying to read save files. Exiting..");
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 * Se i file di salvataggio esistono li carica, altrimenti assume primo avvio
+	 * @throws ClassNotFoundException 
+	 * @throws IOException 
+	 */
+	private void caricaPartitaDaFile() throws IOException, ClassNotFoundException, FileNotFoundException {
+		/**
+		 * Carica file di mappa, se esiste deve esistere anche il file dei giocatori.
+		 * In caso il primo o l'altro non esistano l'eccezione viene gestita e passata al chiamante, che quindi assume un primo avvios
+		 */
+		caricaFileDiMappa("mappa.dat");
+		caricaFileGiocatori("giocatori.dat");
+	}
+	/**
+	 * Implementa il caricamento del file di mappa, se esiste
+	 * Se non esiste lancia FileNotFoundException
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 */
+	private void caricaFileDiMappa(String nomefile) throws IOException, ClassNotFoundException, FileNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(nomefile)));
+		rifMappa = (Mappa) ois.readObject();
+	}
+	/**
+	 * Implementa il caricamento del file giocatori se esiste
+	 * Se non esiste lancia FileNotFoundException
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 */
+	@SuppressWarnings("unchecked")
+	private void caricaFileGiocatori(String nomefile) throws IOException, ClassNotFoundException, FileNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(nomefile)));
+		Giocatori = (Hashtable<String, Giocatore>) ois.readObject();
+	}
+	
+	/**
+	 * Helper per la creazione di una mappa nuova
+	 */
+	private void creaNuovaMappa() {
+		rifMappa = new Mappa(lato_MAPPA);
+	}
 }
--- trunk/src/server/Server.java
+++ trunk/src/server/Server.java
@@ -1,59 +1,26 @@
 package server;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.net.ServerSocket;
-import java.util.Hashtable;
-import java.util.Random;
 
-import dinolib.*;
+import dinolib.Logica;
 
 public class Server {
-	/**
-	 * Definisce staticamente il lato della mappa.
-	 * @uml.property name="LATO_MAPPA"
-	 */
-	private final int lato_MAPPA = 40;
 	/**
 	 * Definisce staticamente la porta di gioco.
 	 * @uml.property name="PORTA_DI_GIOCO"
 	 */
 	private final int PORTA_DI_GIOCO = 32845;
 	/**
-	 * Definisce il riferimento alla mappa.
-	 * @uml.property name="rifMappa"
-	 */
-	protected Mappa rifMappa;
-	/**
-	 * Definisce la lista dei giocatori.
-	 * @uml.property name="Giocatori"
-	 */
-	protected Hashtable<String, Giocatore> Giocatori;
-	/**
-	 * Definisce la stringa che contiene il nome del giocatore che in questo momento ha il turno.
-	 * @uml.property name="Giocatori"
-	 */
-	protected String nomeGiocatoreCorrente;
-	/**
 	 * Definisce il riferimento al socket del server.
 	 * @uml.property name="serverInstSocket"
 	 */
 	private ServerSocket serverInstSocket;
 	/**
-	 * Definisce una variabile che assicura che qualcuno sta giocando.
-	 * @uml.property name="qualcunoStaGiocando"
+	 * Dichiara la classe logica che gestisce tutta la logica di gioco indipendentemente dall'interfaccia di comunicazione.
 	 */
-	protected boolean qualcunoStaGiocando = false;
-
-	/**
-	 * Definisce il generatore di numeri casuali.
-	 * @uml.property name="rnd"
-	 */
-	Random rnd = new Random();
-
+	protected Logica logica = new Logica();
+	
 	/**
 	 * @param args
 	 */
@@ -65,69 +32,8 @@ public class Server {
 	/**
 	 * Dichiara il costruttore dell'oggetto Server che poi gestisce tutto.
 	 */
-	public Server(){
-		try {
-			caricaPartitaDaFile();
-		}
-		catch (FileNotFoundException e) {
-			System.out.println("No save files found, creating a new map..");
-			creaNuovaMappa();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("IOException while trying to open save files. Exiting..");
-			System.exit(-1);
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("ClassNotFoundException while trying to read save files. Exiting..");
-			System.exit(-1);
-		}
+	public Server () {
 		listenSocket();
-	}
-
-	/**
-	 * Implementa Passo 2
-	 * Se i file di salvataggio esistono li carica, altrimenti assume primo avvio
-	 * @throws ClassNotFoundException 
-	 * @throws IOException 
-	 */
-	private void caricaPartitaDaFile() throws IOException, ClassNotFoundException, FileNotFoundException {
-		/**
-		 * Carica file di mappa, se esiste deve esistere anche il file dei giocatori.
-		 * In caso il primo o l'altro non esistano l'eccezione viene gestita e passata al chiamante, che quindi assume un primo avvios
-		 */
-		caricaFileDiMappa("mappa.dat");
-		caricaFileGiocatori("giocatori.dat");
-	}
-
-	/**
-	 * Implementa il caricamento del file di mappa, se esiste
-	 * Se non esiste lancia FileNotFoundException
-	 * @throws IOException 
-	 * @throws ClassNotFoundException 
-	 */
-	private void caricaFileDiMappa(String nomefile) throws IOException, ClassNotFoundException, FileNotFoundException {
-		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(nomefile)));
-		rifMappa = (Mappa) ois.readObject();
-	}
-	/**
-	 * Implementa il caricamento del file giocatori se esiste
-	 * Se non esiste lancia FileNotFoundException
-	 * @throws IOException 
-	 * @throws ClassNotFoundException 
-	 */
-	@SuppressWarnings("unchecked")
-	private void caricaFileGiocatori(String nomefile) throws IOException, ClassNotFoundException, FileNotFoundException {
-		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(nomefile)));
-		Giocatori = (Hashtable<String, Giocatore>) ois.readObject();
-	}
-
-	/**
-	 * Helper per la creazione di una mappa nuova
-	 */
-	private void creaNuovaMappa() {
-		rifMappa = new Mappa(lato_MAPPA);
 	}
 
 	/**
