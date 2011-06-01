40a59e7b1e0bfc8933de57857dd47d6788ff043f
Fabio Scaccabarozzi <fsvm88@users.noreply.github.com>
2011-06-01 14:42:03 +0000

    Fixa altri bug, rimuovi eccezioni inutili.

--- src/dinolib/RaceAlreadyCreatedException.java
+++
@@ -1,9 +0,0 @@
-package dinolib;
-
-@SuppressWarnings("serial")
-class RaceAlreadyCreatedException extends Exception {
-	public RaceAlreadyCreatedException() { }
-	public RaceAlreadyCreatedException(String message) {
-		super(message);
-	}
-}
--- src/dinolib/RaceNameExistsException.java
+++
@@ -1,9 +0,0 @@
-package dinolib;
-
-@SuppressWarnings("serial")
-class RaceNameExistsException extends Exception {
-	public RaceNameExistsException() { }
-	public RaceNameExistsException(String message) {
-		super(message);
-	}
-}
