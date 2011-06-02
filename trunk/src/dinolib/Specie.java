6b06e45f25ac41d989d3b888d34fdf955303a340
fsvm88 <fsvm88@c12e17a0-9a89-4ea4-9972-a45f88b339be>
2011-06-02 18:39:50 +0000

    Continua a implementare la logica di cambio dei turni.

--- trunk/src/dinolib/Specie.java
+++ trunk/src/dinolib/Specie.java
@@ -29,43 +29,55 @@ class Specie {
 	 * @uml.property name="Dinosauri"
 	 */
 	private Hashtable<String, Dinosauro> dinosauri = new Hashtable<String,Dinosauro>();
-	
+
 	/**
 	 * Contiene il punteggio corrente della specie.
 	 * @uml.property name="punteggio"
 	 */
 	private int punteggio = 0;
+
+	/**
+	 * Definisce definitivamente i turni di vita massimi per una specie.
+	 * uml.property name="turni_DI_VITA_MAX"
+	 */
+	private final int TURNI_DI_VITA_MAX = 120;
+	
+	/**
+	 * Contiene i turni di vita della specie.
+	 * uml.property name="turniDiVita"
+	 */
+	private int turniDiVita = 0;
 	
 	/**
 	 * Dice se la razza si è già estinta o meno.
 	 * @uml.property name="isEstinta"
 	 */
 	private boolean isEstinta = false;
-	
+
 	/**
 	 * Helper per verificare se la specie è estinta o meno.
 	 */
 	public boolean isEstinta() {
 		return isEstinta;
 	}
-	
+
 	/**
 	 * Restituisce il punteggio corrente della specie.
 	 */
 	public int getPunteggio() {
 		return punteggio;
 	}
-	
+
 	/**
 	 * Aggiorna il punteggio della specie.
 	 */
-	public void aggiornaPunteggio() {
+	private void aggiornaPunteggio() {
 		Iterator<String> itDinosauri = getItIdDinosauri();
 		while (itDinosauri.hasNext()) {
 			punteggio = (punteggio + (1+dinosauri.get(itDinosauri.next()).getDimensione())); 
 		}
 	}
-	
+
 	/**
 	 * Aggiunge un dinosauro alla lista di dinosauri della Specie.
 	 */
@@ -77,11 +89,11 @@ class Specie {
 			dinosauri.put(nuovoIdDinosauro, dinosauro);
 		}
 	}
-	
+
 	private void laRazzaEEstinta() {
 		isEstinta = true;
 	}
-	
+
 	/**
 	 * Uccide la razza di dinosauri.
 	 * Imposta isEstinta a true e dealloca gli oggetti.
@@ -121,7 +133,7 @@ class Specie {
 		this.nomeRazza = nomeRazza;
 		aggiungiDinosauro(nuovodinosauro, null);
 	}
-	
+
 	/**
 	 * Restituisce il dinosauro nella specie che ha un certo id.
 	 * @param idDinosauroCercato
@@ -130,21 +142,21 @@ class Specie {
 	protected Dinosauro getDinosauro(String idDinosauroCercato) {
 		return dinosauri.get(idDinosauroCercato);
 	}
-	
+
 	/**
 	 * Restituisce un iteratore sui dinosauri presenti nella specie.
 	 */
 	public Iterator<Dinosauro> getItDinosauri() {
 		return dinosauri.values().iterator();
 	}
-	
+
 	/**
 	 * Restituisce un iteratore sugli id dei dinosauri. Utile solo per inviare la lista dei dinosauri all'utente.
 	 */
 	public Iterator<String> getItIdDinosauri() {
 		return dinosauri.keySet().iterator();
 	}
-	
+
 	/**
 	 * Helper booleano per verificare l'esistenza di un id (cioè l'esistenza di un dinosauro.
 	 * @param idDaCercare
@@ -154,7 +166,7 @@ class Specie {
 		if (dinosauri.containsKey(idDaCercare)) return true;
 		else return false;
 	}
-	
+
 	/**
 	 * Helper per restituire il tipo della razza del dinosauro.
 	 */
@@ -164,5 +176,24 @@ class Specie {
 			return itTemp.next().getTipoRazza();
 		}
 		return null;
+	}
+	
+	/**
+	 * Helper per invecchiare i dinosauri nella specie.
+	 */
+	private void invecchiaDinosauri() {
+		Iterator<Dinosauro> itDinosauri = getItDinosauri();
+		while (itDinosauri.hasNext()) {
+			itDinosauri.next().invecchia();
+		}
+	}
+	
+	/**
+	 * Helper per aggiornare la specie.
+	 */
+	protected void aggiornaSpecie() {
+		if (turniDiVita >= TURNI_DI_VITA_MAX) uccidiRazza();
+		aggiornaPunteggio();
+		invecchiaDinosauri();
 	}
 }
