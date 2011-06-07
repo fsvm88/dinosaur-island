71bcfedb9b63dc6481947f705e6e8a6d32c5b1d4
fsvm88 <fsvm88@c12e17a0-9a89-4ea4-9972-a45f88b339be>
2011-06-07 17:51:38 +0000

    Crea test suite JUnit 4 così da raggruppare tutti i test.

---
+++ trunk/src/dinolib/DinolibTest.java
@@ -0,0 +1,17 @@
+package dinolib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({DinosauroTest.class, CellaTest.class})
+public class DinolibTest {
+
+	public static Test suite() {
+		return null;
+		
+	}
+
+}
