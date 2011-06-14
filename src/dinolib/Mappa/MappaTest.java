fe76ef86a24a037ec38cb40f4790f78a966b8417
Fabio Scaccabarozzi <fsvm88@users.noreply.github.com>
2011-06-14 21:46:30 +0000

    Reimplementa allocaAcqua in Mappa e usa HashSet invece di ArrayList. Crea MappaTest come stub.

---
+++ src/dinolib/Mappa/MappaTest.java
@@ -0,0 +1,23 @@
+package dinolib.Mappa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author  fabio
+ */
+public class MappaTest {
+	private Mappa testMappa = null;
+	
+	@Before
+	public void setUp() {
+		testMappa = new Mappa(40);
+	}
+	
+	@Test
+	public void testAll() {
+		
+	}
+}
