863ff40fd0a1f1cd9c4a2d557b2673f04a5e7ef4
Fabio Scaccabarozzi <fsvm88@users.noreply.github.com>
2011-06-14 14:49:33 +0000

    Comincia a reimplementare LogicaTest.

--- src/dinolib/LogicaTest.java
+++ src/dinolib/LogicaTest.java
@@ -2,84 +2,18 @@ package dinolib;
 
 import static org.junit.Assert.*;
 
-import java.util.Hashtable;
-
 import org.junit.Before;
 import org.junit.Test;
 
-import dinolib.Exceptions.UserExistsException;
-import dinolib.Mappa.Cella;
-
 public class LogicaTest {
-	private Logica myLogica = null;
-	private Hashtable<String, Giocatore> testGiocatori = null;
-	
-	private void addSomeUsers() throws UserExistsException {
-		myLogica.doCreaUtente("abc", "pwd");
-		myLogica.doCreaUtente("abd", "pwd2");
-		myLogica.doCreaUtente("abe", "pwd3");
-		testGiocatori.put(CommonUtils.getNewToken(), myLogica.getPlayerByName("abc"));
-		testGiocatori.put(CommonUtils.getNewToken(), myLogica.getPlayerByName("abd"));
-		testGiocatori.put(CommonUtils.getNewToken(), myLogica.getPlayerByName("abe"));
-	}
-	private void mySetUp() throws Exception {
-		setUp();
-		addSomeUsers();
-	}
+	private Logica logicaTest = null;
 	
 	@Before
-	public void setUp() throws Exception {
-		myLogica = new Logica();
-		testGiocatori = new Hashtable<String, Giocatore>();
-	}
-	
-	private void testLogica() throws Exception {
-		setUp();
-		assertNotNull(myLogica);
-	}
-	
-	private void testGetLatoDellaMappa() {
-		assertNotNull(myLogica.getLatoDellaMappa());
-		assertEquals(40, myLogica.getLatoDellaMappa());
-	}
-	private void testGetCella() {
-		Cella tempCella = myLogica.getCella(0, 0);
-		assertNotNull(tempCella);
-	}
-	
-	private void testExistsPlayerWithName() {
-		assertTrue(myLogica.existsPlayerWithName("abc"));
-		assertFalse(myLogica.existsPlayerWithName("abf"));
-	}
-	
-	private void testGetPlayerByName() {
-		assertNotNull(myLogica.existsPlayerWithName("abc"));
-		assertNull(myLogica.existsPlayerWithName("abf"));
-	}
-	
-	private void testGetIteratorS() {
-		assertNotNull(myLogica.getIteratorOnPIds());
-		assertNotNull(myLogica.getIteratorOnPlayers());
-	}
-	
-	private void testDoLoginUtente() {
-		assertFalse(myLogica.getPlayerByName("abc").isLogged());
-		assertFalse(myLogica.getPlayerByName("abd").isLogged());
-		assertFalse(myLogica.getPlayerByName("abe").isLogged());
-		
-
+	public void setUp() {
+		logicaTest = new Logica();
 	}
 	
 	@Test
 	public void testAllLogica() throws Exception {
-		testLogica();
-		testGetLatoDellaMappa();
-		testGetCella();
-		mySetUp();
-		testExistsPlayerWithName();
-		testGetPlayerByName();
-		testGetIteratorS();
-		testDoLoginUtente();
-		
 	}
 }
