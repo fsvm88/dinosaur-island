ef0f3d9fe0a11952f34f6eb29e36b31290deb63c
fsvm88 <fsvm88@c12e17a0-9a89-4ea4-9972-a45f88b339be>
2011-06-18 07:52:31 +0000

    Documenta Carnivoro e Erbivoro.

--- trunk/src/dinolib/Razza/Carnivoro.java
+++ trunk/src/dinolib/Razza/Carnivoro.java
@@ -4,10 +4,14 @@ import dinolib.Mappa.Coord;
 
 public class Carnivoro extends Dinosauro {
 	/**
+	 * Definisce in modo statico e definitivo lo spostamento massimo per un dinosauro di questo tipo.
+	 * Viene passato al costruttore e poi non serve più.
 	 * @uml.property  name="SPOSTAMENTO_MAX" readOnly="true"
 	 */
 	private static final int spostamento_MAX = 3;
 	/**
+	 * Definisce in modo statico e definitivo il moltiplicatore della forza per un dinosauro di questo tipo.
+	 * Viene passato al costruttore e poi non serve più.
 	 * @uml.property  name="MOLTIPLICATORE_FORZA" readOnly="true"
 	 */
 	private static final int moltiplicatore_FORZA = 2;
