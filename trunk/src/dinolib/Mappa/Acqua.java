b8dbbe8141f0b847c4af52112a85a4c9db6b9998
fsvm88 <fsvm88@c12e17a0-9a89-4ea4-9972-a45f88b339be>
2011-06-19 19:12:00 +0000

    Documenta Acqua, Carogna, Terra e Vegetazione. Fixa Coord.

--- trunk/src/dinolib/Mappa/Acqua.java
+++ trunk/src/dinolib/Mappa/Acqua.java
@@ -1,5 +1,11 @@
 package dinolib.Mappa;
-
+/**
+ * @author fabio
+ */
+/**
+ * Classe per la cella Acqua.
+ * Eredita tutti i metodi dalla superclasse, è uno stub solo per esporre il nome.
+ */
 
 class Acqua extends Cella {
 }
--- trunk/src/dinolib/Mappa/Carogna.java
+++ trunk/src/dinolib/Mappa/Carogna.java
@@ -1,9 +1,14 @@
 package dinolib.Mappa;
-
-
+/**
+ * @author fabio
+ */
+/**
+ * Classe per la cella Carogna.
+ * Eredita parte dei metodi dalla superclasse. Fa l'override di altri implementandoli.
+ */
 class Carogna extends Cella {
 	/**
-	 * Il costruttore si occupa di inizializzare il valore di tettoMassimo.
+	 * Il costruttore si occupa di prendere in input il valore massimo per la cella e impostarlo nell'oggetto.
 	 */
 	protected Carogna(int nuovoValore){
 		super.valoreIniziale = nuovoValore;
@@ -24,7 +29,8 @@ class Carogna extends Cella {
 		}
 	}
 	/**
-	 * Override del metodo della classe cella.
+	 * Override del metodo della classe Cella.
+	 * Restituisce il valore attuale della cella.
 	 */
 	@Override
 	public int getValoreAttuale() { return valoreAttuale; }
