c06dcf294764852b7ec0c3d53b4e440734b22b6d
fsvm88 <fsvm88@c12e17a0-9a89-4ea4-9972-a45f88b339be>
2011-06-25 11:24:57 +0000

    Implementa molti metodi di RMIAdapter che hanno solo valori di ritorno booleani e eccezioni.

--- trunk/src/dinolib/Adapter.java
+++ trunk/src/dinolib/Adapter.java
@@ -1,23 +1,30 @@
 package dinolib;
 
+import dinolib.Exceptions.GenericDinosauroException;
+import dinolib.Exceptions.InvalidTokenException;
+import dinolib.Exceptions.NomeRazzaOccupatoException;
+import dinolib.Exceptions.RazzaNonCreataException;
+import dinolib.Exceptions.TroppiGiocatoriException;
+import dinolib.Exceptions.UserAuthenticationFailedException;
+import dinolib.Exceptions.UserExistsException;
 import dinolib.Mappa.Coord;
 
 interface Adapter {
-	Object creaUtente(String nomeUtente, String passwordUtente);
-	Object loginUtente(String nomeUtente, String passwordUtente);
-	Object creaRazza(String token, String nomeRazza, Character tipo);
-	Object accessoPartita(String token);
-	Object uscitaPartita(String token);
+	Object creaUtente(String nomeUtente, String passwordUtente) throws UserExistsException;
+	Object loginUtente(String nomeUtente, String passwordUtente) throws UserAuthenticationFailedException;
+	Object creaRazza(String token, String nomeRazza, Character tipo) throws NomeRazzaOccupatoException, InvalidTokenException;
+	Object accessoPartita(String token) throws InvalidTokenException, TroppiGiocatoriException, RazzaNonCreataException, InterruptedException;
+	Object uscitaPartita(String token) throws InvalidTokenException;
 	Object listaGiocatori(String token);
 	Object classifica(String token);
-	Object logoutUtente(String token);
+	Object logoutUtente(String token) throws InvalidTokenException;
 	Object mappaGenerale(String token);
 	Object listaDinosauri(String token);
 	Object vistaLocale(String token, String idDinosauro);
 	Object statoDinosauro(String token, String idDinosauro);
-	Object muoviDinosauro(String token, String idDinosauro, Coord newCoord);
-	Object cresciDinosauro(String token, String idDinosauro);
-	Object deponiUovo(String token, String idDinosauro);
+	Object muoviDinosauro(String token, String idDinosauro, Coord newCoord) throws InvalidTokenException, GenericDinosauroException;
+	Object cresciDinosauro(String token, String idDinosauro) throws InvalidTokenException, GenericDinosauroException;
+	Object deponiUovo(String token, String idDinosauro) throws GenericDinosauroException, InvalidTokenException;
 	Object confermaTurno(String token);
 	Object passaTurno(String token);
 }
---
+++ trunk/src/dinolib/RMIAdapter.java
@@ -0,0 +1,115 @@
+package dinolib;
+
+import dinolib.Exceptions.*;
+import dinolib.Mappa.Coord;
+
+public class RMIAdapter implements Adapter {
+	/**
+	 * Contiene la classe logica che viene istanziata da server.
+	 * @uml.property  name="myLogica"
+	 * @uml.associationEnd  
+	 */
+	private Logica myLogica = null;
+	
+	/**
+	 * Costruttore pubblico, riceve come unico argomento la classe Logica che deve gestire.
+	 * @param newLogica
+	 */
+	public RMIAdapter (Logica newLogica) {
+		myLogica = newLogica;
+	}
+
+	@Override
+	public Object creaUtente(String nomeUtente, String passwordUtente) throws UserExistsException {
+		return myLogica.doCreaUtente(nomeUtente, passwordUtente);
+	}
+
+	@Override
+	public Object loginUtente(String nomeUtente, String passwordUtente) throws UserAuthenticationFailedException {
+		if (myLogica.doLogin(nomeUtente, passwordUtente)) { return myLogica.getCMan().getToken(nomeUtente); }
+		else { return null; }
+	}
+
+	@Override
+	public Object creaRazza(String token, String nomeRazza, Character tipo) throws NomeRazzaOccupatoException, InvalidTokenException {
+		return myLogica.doCreaRazza(token, nomeRazza, tipo);
+	}
+
+	@Override
+	public Object accessoPartita(String token) throws InvalidTokenException, TroppiGiocatoriException, RazzaNonCreataException, InterruptedException {
+		return myLogica.doAccessoPartita(token);
+	}
+
+	@Override
+	public Object uscitaPartita(String token) throws InvalidTokenException {
+		return myLogica.doUscitaPartita(token);
+	}
+
+	@Override
+	public Object listaGiocatori(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object classifica(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object logoutUtente(String token) throws InvalidTokenException {
+		return myLogica.doLogout(token);
+	}
+
+	@Override
+	public Object mappaGenerale(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object listaDinosauri(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object vistaLocale(String token, String idDinosauro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object statoDinosauro(String token, String idDinosauro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object muoviDinosauro(String token, String idDinosauro, Coord newCoord) throws InvalidTokenException, GenericDinosauroException {
+		return myLogica.doMuoviDinosauro(token, idDinosauro, newCoord);
+	}
+
+	@Override
+	public Object cresciDinosauro(String token, String idDinosauro) throws InvalidTokenException, GenericDinosauroException {
+		return myLogica.doCresciDinosauro(token, idDinosauro);
+	}
+
+	@Override
+	public Object deponiUovo(String token, String idDinosauro) throws GenericDinosauroException, InvalidTokenException {
+		return myLogica.doDeponiUovo(token, idDinosauro);
+	}
+
+	@Override
+	public Object confermaTurno(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object passaTurno(String token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
